4ac41ac77150db64160a53b770e7c8b3fe069a7c
Sayeed Joseph <sayeedjoseph@gmail.com>
2020-04-09 23:24:11 -0400

    chore: add session and passport

--- server/server.js
+++ server/server.js
@@ -1,99 +1,68 @@
 const Koa = require('koa');
-const json = require('koa-json');
-const bodyParser = require('koa-bodyparser');
+const { db } = require('./db/');
 const logger = require('koa-logger');
 const static = require('koa-static');
-// const send = require('koa-send');
+const session = require('koa-session');
 const passport = require('koa-passport');
-const session = require('koa-generic-session')
-const { db } = require('./db/');
-const User = require('./db/models/User');
-// const SequelizeStore = require('koa-generic-session-sequelize');
-// const dbStore = new SequelizeStore({ db });
-
-// dbStore.sync();
-
-//Intialize app.  
-const app =  new Koa();
-const PORT = 3000; 
-
+const bodyParser = require('koa-bodyparser');
 
 //Sub-route folders
 const apiRouter = require('./api');
 const authRouter = require('./auth');
 
-passport.serializeUser((user, done) => {
-  try {
-    done(null, user.id);
-  } catch (error) {
-    done(error);
-  }
-});
 
-passport.deserializeUser(async (id, done) => {
-  try {
-    const user = await User.findByPk(id);
-    done(null, user);
-  } catch (err) {
-    done(err);
-  }
-});
+//Intialize app.  
+const app =  new Koa();
+const PORT = process.env.PORT ||3000; 
 
-// // sessions
-// app.keys = [ process.env.SESSION_SECRET || 'a wildly insecure secret' ];
-// app.use(session({
-//   store: dbStore, 
-// }));
-
-
-
-app.use(passport.initialize());
-app.use(passport.session());
-
+// Session CONFIG
+const CONFIG =  {
+  key: 'koa:sess',
+  maxAge: 86400000,
+  overwrite: true,
+  httpOnly: true,
+  signed: false,
+  rolling: false,
+  renew: false
+}
 
 // Logger Middleware
 app.use(logger());
-// JSON Prettier Middleware
-app.use(json());
 // Body Parser Middleware
 app.use(bodyParser());
 
+// Error Middleware
+app.use(async (ctx, next) => {
+  try {
+    console.log('here')
+    await next();
+  } catch (err) {
+    ctx.throw(err.status || 500, err.message || 'Internal Server Error');
+  }
+});
 
-// // Router Middleware
-app.use(apiRouter.routes()).use(apiRouter.allowedMethods());
-app.use(authRouter.routes()).use(apiRouter.allowedMethods());
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
+passport.deserializeUser(async (id, done) => {
+  const user = await User.findByPk(id);
+  done(null, user);
+});
+
+app.use(session(CONFIG, app));
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 // Static middlware
 app.use(static('./public'));
 
 
-// app.use(async (ctx) => {
-//   await send(ctx, ctx.path, { root: __dirname + '/public/index.html ' });
-// }) 
+// // Router Middleware
+app.use(apiRouter.routes());
+app.use(authRouter.routes());
 
-// // 
-// app.use(async (ctx, next) => {
-//   try {
-//     ctx.body = 'Hello World!'
-//     await next();
-//   } catch (error) {
-//     console.log('Passing error to error middlware...')
-//     next(error)
-//   }
-// });
-
-// Error Middleware
-app.use(async (next, ctx) => { 
-  try {
-    console.log('here');
-    await next()
-  } catch (err) {
-    console.log('Recieving error...')
-    console.log(err.status)    
-    ctx.status = err.status || 500;
-    ctx.body = err.message;
-  }
-})
 
 // // Start the app..
 // app.listen(PORT, () => console.log(`Server is listening on PORT: ${PORT}`));

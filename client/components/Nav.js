412a377c13ee98901fc7ef3ebf7560625a8fece3
Sayeed Joseph <sayeedjoseph@gmail.com>
2020-04-10 00:25:57 -0400

    chore: add Nav bar

---
+++ client/components/Nav.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Nav = () => { 
+  <div>
+    <nav className="level">
+      <div className="level-left">
+      </div>
+      <div className="level-right">
+        <p className="level-item">
+          <Link to='/'>
+            <u>Home</u>
+          </Link>
+        </p>
+        <p className="level-item">
+          <Link to='/signin'>
+            <u>Sign In</u>
+          </Link>
+        </p>
+        <p className="level-item">
+          <Link to='/register'>
+            <u>Register</u>
+          </Link>
+        </p>
+      </div>
+    </nav>
+  </div>
+}
+
+
+export default Nav;
